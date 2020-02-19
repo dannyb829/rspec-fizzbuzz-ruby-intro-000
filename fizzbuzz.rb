f8c307e2be2342fd05c1df73b00397078b492978
Daniel Brito <dannyb829@gmail.com>
2020-02-19 23:21:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(int)
     return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 3 and 5 == 0
+  elsif int % 3 == 0 && int % 5 == 0
     return "Fizbuzz"
   else int % 3 != 0 || int % 5 != 0
     return nil
